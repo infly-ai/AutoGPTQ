378ed17b3464bdccddda03c48e48983a728c783c
kelang.tkl <kelang.tkl@inftech.ai>
2024-07-18 03:28:42 +0000

    support inflm

--- auto_gptq/modeling/auto.py
+++ auto_gptq/modeling/auto.py
@@ -29,7 +29,8 @@ from .stablelmepoch import StableLMEpochGPTQForCausalLM
 from .starcoder2 import Starcoder2GPTQForCausalLM
 from .xverse import XverseGPTQForCausalLM
 from .yi import YiGPTQForCausalLM
-
+from .inflm import InfLMGPTQForCausalLM
+from .llamax import LlamaxGPTQForCausalLM
 
 GPTQ_CAUSAL_LM_MODEL_MAP = {
     "bloom": BloomGPTQForCausalLM,
@@ -60,6 +61,7 @@ GPTQ_CAUSAL_LM_MODEL_MAP = {
     "gemma": GemmaGPTQForCausalLM,
     "phi": PhiGPTQForCausalLM,
     "mpt": MPTGPTQForCausalLM,
+    "inflm": InfLMGPTQForCausalLM,
 }
 
 
